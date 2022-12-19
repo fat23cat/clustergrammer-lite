9f0b0f964fde460ae976e391dd45fd7aa19a7c12
Dmitrii Gurev <dmitrii_gurev@epam.com>
2022-12-19 17:36:45 +0300

    It almost works

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import css from 'rollup-plugin-import-css';
 import pkg from './package.json' assert { type: 'json' };
+import css from 'rollup-plugin-import-css';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default [
   // browser-friendly UMD build
   {
     input: 'src/main.js',
+    external: ['d3', 'jQuery'],
     output: {
-      name: 'clustergrammer-lite',
+      name: 'clustergrammer',
       file: pkg.browser,
       format: 'umd'
     },
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      css()
-    ]
+    plugins: [resolve(), commonjs(), css()]
   },
-
   // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/main.js',
-    external: ['ms'],
+    external: ['d3', 'jQuery'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
-    ]
+    ],
+    plugins: [resolve(), commonjs(), css(), visualizer()]
   }
 ];
