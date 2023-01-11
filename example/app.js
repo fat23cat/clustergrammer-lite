40f10b2283e83de16c4735f2ce37284505b4143f
Dmitrii Gurev <dmitrii_gurev@epam.com>
2023-01-11 18:41:33 +0300

    Add sample data

--- example/app.js
+++ example/app.js
@@ -1,11 +1,43 @@
 import Clastergrammer from '../src/main.js';
 
+function generateData() {
+  const rowCount = 500;
+  const colCount = 100;
+  const nGroupDepth = 10;
+  const linkCount = 10000;
+  const network_data = {
+    row_nodes: Array(rowCount)
+      .fill(0)
+      .map((_, i) => ({
+        name: `ROW-${i}`,
+        clust: i,
+        group: Array(nGroupDepth)
+          .fill(0)
+          .map((_, d) => parseInt(i / 2 ** d + ''))
+      })),
+    col_nodes: Array(colCount)
+      .fill(0)
+      .map((_, i) => ({
+        name: `COL-${i}`,
+        clust: i,
+        group: Array(nGroupDepth)
+          .fill(0)
+          .map((_, d) => parseInt(i / 2 ** d + ''))
+      })),
+    links: Array(linkCount)
+      .fill(0)
+      .map((_) => ({
+        source: Math.floor(Math.random() * rowCount),
+        target: Math.floor(Math.random() * colCount),
+        value: Math.round(Math.random())
+      }))
+  };
+  return network_data;
+}
+
+const network_data = generateData();
 const clastergrammer = Clastergrammer({
   root: '#clust_container',
-  network_data: {
-    row_nodes: [{ name: 'FOO' }, { name: 'BAR' }],
-    col_nodes: [{ name: 'BOO' }, { name: 'BAZ' }],
-    links: []
-  },
+  network_data,
   tile_colors: ['#2F80ED', '#2F80ED']
 });
